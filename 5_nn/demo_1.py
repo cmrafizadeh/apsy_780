f67f3d9ae9846273c5e9f65ec6c0e18b44eba7ad
Gaurav Malhotra <gmalhotra@albany.edu>
2025-11-24 11:46:15 -0500

    Module 5: Demo 1 & slides

---
+++ 5_nn/demo_1.py
@@ -0,0 +1,21 @@
+import torch
+import numpy as np
+
+# a = 2
+# a = np.array([2.0])
+a = torch.tensor(2.0, requires_grad=True) # 1D tensor
+b = torch.tensor(1.0, requires_grad=True)
+
+c = a + b
+d = b + 1
+e = c * d
+
+print("Tensor a: ", a)
+print("Tensor b: ", b)
+print("Tensor c: ", c)
+print("Tensor d: ", d)
+print("Tensor e: ", e)
+
+e.backward()
+print(f"Gradient of e wrt a = {a.grad}")
+print(f"Gradient of e wrt b = {b.grad}")
